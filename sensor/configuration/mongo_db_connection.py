f29fc5260a1c7478d418a958563b9fc1c8cdeb9a
Abdul Jaweed <jdgaming7320@gmail.com>
2022-12-05 19:10:57 +0530

    done

---
+++ sensor/configuration/mongo_db_connection.py
@@ -0,0 +1,20 @@
+import pymongo
+from sensor.constant.database import DATABASE_NAME
+#from sensor.constant.env_variable import MONGODB_URL_KEY
+import certifi
+#import os
+ca = certifi.where()
+
+class MongoDBClient:
+    client = None
+    def __init__(self, database_name=DATABASE_NAME) -> None:
+        try:
+
+            if MongoDBClient.client is None:
+                mongo_db_url = "mongodb+srv://<username>:<password>@ineuron-ai-projects.7eh1w4s.mongodb.net/?retryWrites=true&w=majority"#os.getenv(MONGODB_URL_KEY)
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+        except Exception as e:
+            raise e
